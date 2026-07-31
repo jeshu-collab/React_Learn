7366598606556c1a592ea6338e2fc71dc2ce43f7
jeshu-collab <jeshu241125@gmail.com>
2026-07-31 21:01:19 +0530

    added all colors

--- backgriund changer/back-ground/src/App.jsx
+++ backgriund changer/back-ground/src/App.jsx
@@ -1,3 +1,4 @@
+
 import { useState } from "react";
 
 function App() {
@@ -12,9 +13,25 @@ function App() {
 
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 gap-2">
         <div className="flex flex-wrap justify-center gap-2 shadow-lg bg-white p-3 rounded-xl">
-          test
+          <button className="outline-none px-4 py-2 bg-red-500 text-white rounded-lg" onClick={() => setColor("red")}>
+            Red
+          </button>
+          <button className="outline-none px-4 py-2 bg-green-500 text-white rounded-lg" onClick={() => setColor("green")}>
+            Green
+          </button>
+          <button className="outline-none px-4 py-2 bg-blue-500 text-white rounded-lg" onClick={() => setColor("blue")}>
+            Blue
+          </button>
+          <button className="outline-none px-4 py-2 bg-yellow-500 text-white rounded-lg" onClick={() => setColor("yellow")}>
+            Yellow
+          </button>
+          <button className="outline-none px-4 py-2 bg-purple-500 text-white rounded-lg" onClick={() => setColor("purple")}>
+            Purple
+          </button>
         </div>
       </div>
+        
+      
     </>
   );
 }
